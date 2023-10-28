7a5d2131e2834d3b4d293bd058ec4a58e7b245f8
diadatp <git@diadatp.com>
2023-10-28 22:46:03 +0530

    Rename cocotb test.

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 @cocotb.test()
-async def test_single_key(dut):
+async def test_spigot(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 1, units="us")
     cocotb.start_soon(clock.start())
@@ -16,6 +16,7 @@ async def test_single_key(dut):
     dut.uio_in.value = 0
 
     await ClockCycles(dut.clk, 10)
+
     dut.rst_n.value = 1
     dut.ena.value = 1
 
