bb6edcf99a97301c19d7f0a0f5c6a396eb86b7b6
diadatp <git@diadatp.com>
2023-10-28 17:41:32 +0530

    Add skeleton top module and testbench.

---
+++ src/tt_um_diadatp_spigot.v
@@ -0,0 +1,18 @@
+`default_nettype none
+
+module tt_um_diadatp_spigot (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  assign uo_out  = 0;
+  assign uio_oe  = 0;
+  assign uio_out = 0;
+
+endmodule
