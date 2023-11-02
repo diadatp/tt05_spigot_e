f9188ca736ff44fd91fef141b48f1847093dc5ca
diadatp <git@diadatp.com>
2023-11-02 18:25:42 +0530

    Removed fsm, rewrote memory logic and comments.

--- src/TopModule.scala
+++ src/TopModule.scala
@@ -6,7 +6,7 @@ import spinal.lib._
 import spinal.lib.fsm._
 import spinal.lib.sim._
 
-case class TopModule(n: Int, dataWidth: BitCount = 8 bits) extends Component {
+case class TopModule(n: Int, dataWidth: BitCount = 8 bits, period: Int = 14) extends Component {
   val io = new Bundle {
     val ena = in Bool ()
     val digit0 = out UInt (4 bits)
@@ -17,106 +17,82 @@ case class TopModule(n: Int, dataWidth: BitCount = 8 bits) extends Component {
 
   val digits = Vec.fill(4)(Reg(UInt(4 bits)) init (0))
 
-  val mem = Mem(wordType = UInt(dataWidth), wordCount = (n + 1))
+  // outer loop runs (n - 1) times from 0 to (n - 2)
+  val outerCounter = Reg(UInt(log2Up(n - 1) bits)) init (0)
 
-  // when(io.ena) {
-  // {
-  val outerFsm = new StateMachine {
-    // outer loop runs (n - 1) times from 0 to (n - 2)
-    val outerCounter = Reg(UInt(log2Up(n - 1) bits)) init (0)
-    // inner loop runs (n + 1) times from 0 to n
-    val innerCounter = Reg(UInt(log2Up(n + 1) bits)) init (0)
+  // inner loop runs (n + 1) times from (n + 2) to 2
+  val innerCounter = Reg(UInt(log2Up(n + 3) bits)) init (n + 2)
 
-    // divisor goes from 2 to (n + 2)
-    val divisor = Reg(UInt(log2Up(n + 3) bits)) init (0)
-    val dividend = Reg(UInt(dataWidth)) init (0)
-    val divideCounter = Reg(UInt(log2Up(dividend.getWidth + 1) bits)) init (0)
-    val accumulator = Reg(UInt(dataWidth + dataWidth)) init (0)
-    val quotient = Reg(UInt(dataWidth)) init (0)
-    val remainder = Reg(UInt(dataWidth)) init (0)
+  // sliding window from 0 to (period - 1)
+  val slideCounter = CounterFreeRun(period)
 
-    val stateInit: State = new State with EntryPoint {
-      onEntry {
-        innerCounter := n
-        outerCounter := (n - 2)
+  val dataIsValid = (slideCounter === 0)
+  val startOfInnerLoop = (innerCounter === (n + 2))
+  val startOfOuterLoop = (outerCounter === 0)
+
+  val mem = Vec(Reg(UInt(dataWidth)) init (1), n + 1)
+
+  when(slideCounter.willOverflow) {
+    when(innerCounter === 2) {
+      when(outerCounter === (n - 2)) {
+        // do nothing?
+      } otherwise {
+        innerCounter := n + 2
+        outerCounter := outerCounter + 1
       }
-      whenIsActive {
-        mem.write(innerCounter, U(1, dataWidth))
-
-        when(innerCounter === 0) {
-          goto(stateMulTen)
-        } otherwise {
-          innerCounter := innerCounter - 1
-        }
-      }
-      onExit(digits(0) := 2)
+    } otherwise {
+      innerCounter := innerCounter - 1
     }
-
-    val stateMulTen: State = new State {
-      onEntry(innerCounter := n)
-      whenIsActive {
-        mem.write(innerCounter, (10 * mem.readAsync(innerCounter))(7 downto 0))
-        when(innerCounter === 0) {
-          goto(stateDivideInit)
-        } otherwise {
-          innerCounter := innerCounter - 1
-        }
-      }
-      onExit(innerCounter := n)
-    }
-
-    val stateDivideInit: State = new State {
-      whenIsActive {
-        divideCounter := dividend.getWidth
-        divisor := (innerCounter + 2)
-        accumulator := mem.readAsync(innerCounter).resize(dataWidth + dataWidth)
-        goto(stateDivDo)
-      }
-    }
-
-    val stateDivDo: State = new State {
-      whenIsActive {
-        when(divideCounter === 0) {
-          quotient := accumulator(7 downto 0)
-          remainder := accumulator(15 downto 8)
-          goto(stateCommitDivide)
-        } otherwise {
-          divideCounter := divideCounter - 1
-        }
-        when(accumulator(15 downto 7) >= divisor) {
-          accumulator := (accumulator(15 downto 7) - divisor) (7 downto 0) @@ accumulator(6 downto 0) @@ U(1)
-        } otherwise {
-          accumulator := accumulator |<< 1
-        }
-      }
-    }
-
-    val stateCommitDivide: State = new State {
-      whenIsActive {
-        innerCounter := innerCounter - 1
-        mem.write(innerCounter, remainder)
-        mem.write(innerCounter - 1, (mem.readAsync(innerCounter - 1) + quotient))
-
-        when(innerCounter === 0) {
-          digits(0) := quotient.resize(4 bits)
-          for (i <- 1 to 3) {
-            digits(i) := digits(i - 1)
-          }
-          when(outerCounter === 0) {
-            goto(stateFinish)
-          } otherwise {
-            outerCounter := outerCounter - 1
-            goto(stateMulTen)
-          }
-        } otherwise {
-          goto(stateDivideInit)
-        }
-      }
-    }
-
-    val stateFinish: State = new State
   }
-  // }
+
+  when(slideCounter.willOverflow) {
+    mem(n) := mem(0)
+    for (i <- 1 to n) {
+      mem(i - 1) := mem(i)
+    }
+  }
+
+  val opWidth = ((UInt(dataWidth) * 10) +^ UInt(dataWidth)).getWidth
+
+  val accumulator = Reg(UInt((2 * opWidth) bits)) init (0)
+
+  val quotient = accumulator(0, dataWidth)
+  val remainder = accumulator(opWidth, dataWidth)
+
+  val accumulator_lshift = accumulator |<< 1
+  val diff = accumulator_lshift(((2 * opWidth) - 1) downto opWidth) - innerCounter
+
+  when(dataIsValid) {
+
+    when(startOfInnerLoop) {
+      // the input quotient is zero at the start of the loop
+      accumulator := (mem(0) * 10).resized
+    } otherwise {
+      // get input quotient from previous division
+      accumulator := ((mem(0) * 10) +^ quotient).resized
+    }
+
+    // store the remainder except when the algorithm starts
+    when(~(startOfInnerLoop && startOfOuterLoop)) {
+      mem(n) := remainder
+    }
+
+  } otherwise {
+    when(accumulator_lshift(opWidth, opWidth bits) >= innerCounter) {
+      accumulator := diff(0, opWidth bits) @@ accumulator_lshift(1, (opWidth - 1) bits) @@ U(1)
+    } otherwise {
+      accumulator := accumulator_lshift
+    }
+  }
+
+  when(dataIsValid) {
+    when(startOfInnerLoop) {
+      digits(0) := quotient.resized
+      for (i <- 1 to 3) {
+        digits(i) := digits(i - 1)
+      }
+    }
+  }
 
   io.digit0 := digits(0)
   io.digit1 := digits(1)
--- src/TopModule.v
+++ src/TopModule.v
@@ -1,8 +1,6 @@
 // Generator : SpinalHDL v1.9.3    git head : 029104c77a54c53f1edda327a3bea333f7d65fd9
 // Component : TopModule
-// Git hash  : a9dc64edfffc35d69187172cb84fdd999a124fb4
-
-`timescale 1ns/1ps
+// Git hash  : 428ab45aa8807a528d31874b0b0600a666d55d4b
 
 module TopModule (
   input               io_ena,
@@ -16,18 +14,28 @@ module TopModule (
 
   wire       [3:0]    _zz_slideCounter_valueNext;
   wire       [0:0]    _zz_slideCounter_valueNext_1;
-  wire       [8:0]    _zz_dividend;
   wire       [12:0]   _zz_diff;
-  wire       [12:0]   _zz_test;
-  wire       [12:0]   _zz_accumulator;
-  wire       [11:0]   _zz_accumulator_1;
-  wire       [12:0]   _zz_accumulator_2;
-  wire       [8:0]    _zz_accumulator_3;
-  wire       [12:0]   _zz_when_TopModule_l73;
+  wire       [11:0]   _zz_accumulator;
+  wire       [12:0]   _zz_accumulator_1;
+  wire       [11:0]   _zz_accumulator_2;
+  wire       [12:0]   _zz_accumulator_3;
+  wire       [8:0]    _zz_accumulator_4;
+  wire       [12:0]   _zz_when_TopModule_l114;
   reg        [3:0]    digits_0;
   reg        [3:0]    digits_1;
   reg        [3:0]    digits_2;
   reg        [3:0]    digits_3;
+  reg        [3:0]    outerCounter;
+  reg        [3:0]    innerCounter;
+  wire                slideCounter_willIncrement;
+  wire                slideCounter_willClear;
+  reg        [3:0]    slideCounter_valueNext;
+  reg        [3:0]    slideCounter_value;
+  wire                slideCounter_willOverflowIfInc;
+  wire                slideCounter_willOverflow;
+  wire                dataIsValid;
+  wire                startOfInnerLoop;
+  wire                startOfOuterLoop;
   reg        [7:0]    mem_0;
   reg        [7:0]    mem_1;
   reg        [7:0]    mem_2;
@@ -39,42 +47,25 @@ module TopModule (
   reg        [7:0]    mem_8;
   reg        [7:0]    mem_9;
   reg        [7:0]    mem_10;
-  reg        [3:0]    outerCounter;
-  reg        [3:0]    innerCounter;
-  wire                slideCounter_willIncrement;
-  wire                slideCounter_willClear;
-  reg        [3:0]    slideCounter_valueNext;
-  reg        [3:0]    slideCounter_value;
-  wire                slideCounter_willOverflowIfInc;
-  wire                slideCounter_willOverflow;
-  wire                when_TopModule_l31;
-  wire                when_TopModule_l32;
-  wire                when_TopModule_l33;
-  wire                when_TopModule_l44;
-  reg        [7:0]    qin;
-  wire       [12:0]   dividend;
+  wire                when_TopModule_l36;
+  wire                when_TopModule_l37;
   reg        [25:0]   accumulator;
-  wire       [25:0]   accumulator_lshift;
-  wire       [12:0]   diff_l;
-  wire       [3:0]    diff_r;
-  wire       [12:0]   diff;
-  wire                test;
   wire       [7:0]    quotient;
   wire       [7:0]    remainder;
-  wire                when_TopModule_l67;
-  reg        [25:0]   _zz_qin;
-  wire                when_TopModule_l73;
+  wire       [25:0]   accumulator_lshift;
+  wire       [12:0]   diff;
+  wire                when_TopModule_l109;
+  wire                when_TopModule_l114;
 
   assign _zz_slideCounter_valueNext_1 = slideCounter_willIncrement;
   assign _zz_slideCounter_valueNext = {3'd0, _zz_slideCounter_valueNext_1};
-  assign _zz_dividend = ({1'b0,mem_0} + {1'b0,qin});
   assign _zz_diff = {9'd0, innerCounter};
-  assign _zz_test = {9'd0, innerCounter};
-  assign _zz_accumulator = ({1'b0,_zz_accumulator_1} + _zz_accumulator_2);
-  assign _zz_accumulator_1 = (mem_0 * 4'b1010);
-  assign _zz_accumulator_3 = {1'b0,qin};
-  assign _zz_accumulator_2 = {4'd0, _zz_accumulator_3};
-  assign _zz_when_TopModule_l73 = {9'd0, innerCounter};
+  assign _zz_accumulator = (mem_0 * 4'b1010);
+  assign _zz_accumulator_1 = ({1'b0,_zz_accumulator_2} + _zz_accumulator_3);
+  assign _zz_accumulator_2 = (mem_0 * 4'b1010);
+  assign _zz_accumulator_4 = {1'b0,quotient};
+  assign _zz_accumulator_3 = {4'd0, _zz_accumulator_4};
+  assign _zz_when_TopModule_l114 = {9'd0, innerCounter};
   assign slideCounter_willClear = 1'b0;
   assign slideCounter_willOverflowIfInc = (slideCounter_value == 4'b1101);
   assign slideCounter_willOverflow = (slideCounter_willOverflowIfInc && slideCounter_willIncrement);
@@ -90,28 +81,17 @@ module TopModule (
   end
 
   assign slideCounter_willIncrement = 1'b1;
-  assign when_TopModule_l31 = (slideCounter_value == 4'b1101);
-  assign when_TopModule_l32 = (innerCounter == 4'b0010);
-  assign when_TopModule_l33 = (outerCounter == 4'b0000);
-  assign when_TopModule_l44 = (slideCounter_value == 4'b1101);
-  assign dividend = (_zz_dividend * 4'b1010);
-  assign accumulator_lshift = (accumulator <<< 1);
-  assign diff_l = accumulator_lshift[25 : 13];
-  assign diff_r = innerCounter;
-  assign diff = (accumulator_lshift[25 : 13] - _zz_diff);
-  assign test = (_zz_test <= accumulator_lshift[25 : 13]);
-  assign quotient = accumulator[7:0];
+  assign dataIsValid = (slideCounter_value == 4'b0000);
+  assign startOfInnerLoop = (innerCounter == 4'b1100);
+  assign startOfOuterLoop = (outerCounter == 4'b0000);
+  assign when_TopModule_l36 = (innerCounter == 4'b0010);
+  assign when_TopModule_l37 = (outerCounter == 4'b1000);
+  assign quotient = accumulator[7 : 0];
   assign remainder = accumulator[20 : 13];
-  assign when_TopModule_l67 = (slideCounter_value == 4'b0000);
-  assign when_TopModule_l73 = (_zz_when_TopModule_l73 <= accumulator_lshift[25 : 13]);
-  always @(*) begin
-    if(when_TopModule_l73) begin
-      _zz_qin = {{diff[12 : 0],accumulator_lshift[12 : 1]},1'b1};
-    end else begin
-      _zz_qin = accumulator_lshift;
-    end
-  end
-
+  assign accumulator_lshift = (accumulator <<< 1);
+  assign diff = (accumulator_lshift[25 : 13] - _zz_diff);
+  assign when_TopModule_l109 = (! (startOfInnerLoop && startOfOuterLoop));
+  assign when_TopModule_l114 = (_zz_when_TopModule_l114 <= accumulator_lshift[25 : 13]);
   assign io_digit0 = digits_0;
   assign io_digit1 = digits_1;
   assign io_digit2 = digits_2;
@@ -122,6 +102,9 @@ module TopModule (
       digits_1 <= 4'b0000;
       digits_2 <= 4'b0000;
       digits_3 <= 4'b0000;
+      outerCounter <= 4'b0000;
+      innerCounter <= 4'b1100;
+      slideCounter_value <= 4'b0000;
       mem_0 <= 8'h01;
       mem_1 <= 8'h01;
       mem_2 <= 8'h01;
@@ -133,24 +116,20 @@ module TopModule (
       mem_8 <= 8'h01;
       mem_9 <= 8'h01;
       mem_10 <= 8'h01;
-      outerCounter <= 4'b1000;
-      innerCounter <= 4'b1100;
-      slideCounter_value <= 4'b0000;
-      qin <= 8'h00;
       accumulator <= 26'h0000000;
     end else begin
       slideCounter_value <= slideCounter_valueNext;
-      if(when_TopModule_l31) begin
-        if(when_TopModule_l32) begin
-          if(!when_TopModule_l33) begin
+      if(slideCounter_willOverflow) begin
+        if(when_TopModule_l36) begin
+          if(!when_TopModule_l37) begin
             innerCounter <= 4'b1100;
-            outerCounter <= (outerCounter - 4'b0001);
+            outerCounter <= (outerCounter + 4'b0001);
           end
         end else begin
           innerCounter <= (innerCounter - 4'b0001);
         end
       end
-      if(when_TopModule_l44) begin
+      if(slideCounter_willOverflow) begin
         mem_10 <= mem_0;
         mem_0 <= mem_1;
         mem_1 <= mem_2;
@@ -163,16 +142,30 @@ module TopModule (
         mem_8 <= mem_9;
         mem_9 <= mem_10;
       end
-      if(when_TopModule_l67) begin
-        accumulator <= {13'd0, _zz_accumulator};
-      end else begin
-        accumulator <= _zz_qin;
-        qin <= _zz_qin[7:0];
+      if(dataIsValid) begin
+        if(startOfInnerLoop) begin
+          digits_0 <= quotient[3:0];
+          digits_1 <= digits_0;
+          digits_2 <= digits_1;
+          digits_3 <= digits_2;
+        end
       end
-      digits_0 <= quotient[3:0];
-      digits_1 <= digits_0;
-      digits_2 <= digits_1;
-      digits_3 <= digits_2;
+      if(dataIsValid) begin
+        if(startOfInnerLoop) begin
+          accumulator <= {14'd0, _zz_accumulator};
+        end else begin
+          accumulator <= {13'd0, _zz_accumulator_1};
+        end
+        if(when_TopModule_l109) begin
+          mem_10 <= remainder;
+        end
+      end else begin
+        if(when_TopModule_l114) begin
+          accumulator <= {{diff[12 : 0],accumulator_lshift[12 : 1]},1'b1};
+        end else begin
+          accumulator <= accumulator_lshift;
+        end
+      end
     end
   end
 
