236e6ee1ec8b241f6fcda2087fac58dd983cc93b
diadatp <git@diadatp.com>
2023-11-03 21:49:45 +0530

    Improve sim to check digits up to 45th.

--- src/TopModuleSim.scala
+++ src/TopModuleSim.scala
@@ -15,9 +15,25 @@ object TopModuleSim extends App {
 
     dut.io.ena #= true
 
-    var modelState = 0
-    for (idx <- 0 to 200) {
+    val e_str = "271828182845904523536028747135266249775724709"
+    val e_ms = scala.collection.mutable.Queue(e_str.toList: _*)
+    var digitModel = e_ms.dequeue().asDigit
+
+    // approximately twice the clock cycles the design should take
+    val totalCycles = (2 * (6 + dut.dataWidth.value) * dut.n * dut.n)
+
+    var prevDigit = dut.io.digit0.toInt
+
+    for (i <- 0 to totalCycles) {
       dut.clockDomain.waitRisingEdge()
+      if (prevDigit != dut.io.digit0.toInt) {
+        assert(
+          dut.io.digit0.toInt == digitModel,
+          s"dut.io.digit0 mismatch. io.digit0=${dut.io.digit0.toInt} digitModel=$digitModel"
+        )
+        digitModel = e_ms.dequeue().asDigit
+        prevDigit = dut.io.digit0.toInt
+      }
     }
   }
 }
